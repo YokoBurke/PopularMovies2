46541b19e792ac9681560cb63b5a22ac77a7756b
yoko.fujikuro <fujikurock@gmail.com>
2018-06-18 16:00:34 -0400

    Network Util Added

--- app/src/main/java/com/example/jamesburke/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.jamesburke.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+
+import com.example.jamesburke.popularmovies.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        URL myUrl = NetworkUtils.buildURL("polularity.asc");
+
+        String myString = "";
+        try {
+            myString = NetworkUtils.getResponseFromHttpUrl(myUrl);
+        } catch (IOException e){
+            Log.e("Main Activity", "Problem making the HTTP request.", e);
+        }
+
+        Log.i("Information", myString);
     }
 
     @Override
---
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,79 @@
+package com.example.jamesburke.popularmovies.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkUtils {
+
+    final static String CLASS_NAME = String.class.getSimpleName();
+
+    final static String MOVIEDB_BASE_URL = "https://api.themoviedb.org/3/discover/movie?api_key=";
+    final static String API_URL = "73e4a66f623745e5138464b4ac6fb93b";
+
+    final static String PARAM_SORT = "sort_by";
+    final static String PARAM_PAGE = "page";
+    final static String pageNumber = "1";
+
+    public static URL buildURL(String sortOrder) {
+
+        Uri builtUri = Uri.parse(MOVIEDB_BASE_URL + API_URL).buildUpon().appendQueryParameter(PARAM_SORT, sortOrder)
+                .appendQueryParameter(PARAM_PAGE, pageNumber)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+       String jsonResponse = "";
+       HttpURLConnection urlConnection = null;
+       InputStream inputStream = null;
+
+       try {
+           urlConnection = (HttpURLConnection) url.openConnection();
+           urlConnection.setRequestMethod("GET");
+           urlConnection.setReadTimeout(10000);
+           urlConnection.setConnectTimeout(15000);
+           urlConnection.connect();
+
+           if (urlConnection.getResponseCode() == 200) {
+               inputStream = urlConnection.getInputStream();
+               BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
+               StringBuilder total = new StringBuilder();
+
+               while ((jsonResponse = r.readLine()) != null) {
+                   total.append(jsonResponse).append('\n');
+               }
+           } else {
+               Log.e(CLASS_NAME, "Error response code: " + urlConnection.getResponseCode());
+           }
+
+       }catch (IOException e) {
+           Log.e(CLASS_NAME , "Problem retrieving the JSON result.", e);
+       } finally {
+           if (urlConnection != null) {
+               urlConnection.disconnect();
+           }
+           if (inputStream != null) {
+
+               inputStream.close();
+           }
+       }
+
+       return jsonResponse;
+    }
+}
