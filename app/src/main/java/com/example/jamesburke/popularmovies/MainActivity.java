63750c27bd375abb9c10fdce65a6115f996dfdf4
yoko.fujikuro <fujikurock@gmail.com>
2018-06-20 15:12:59 -0400

    temp

--- app/src/main/java/com/example/jamesburke/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jamesburke.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,22 +14,49 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MyActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        URL myUrl = NetworkUtils.buildURL("polularity.asc");
+        MovieAsyncTask task = new MovieAsyncTask();
+        task.execute();
 
+    }
+
+    public class MovieAsyncTask extends AsyncTask<URL, Void, String> {
+
+
+        URL myUrl = NetworkUtils.buildURL("polularity.asc");
         String myString = "";
-        try {
-            myString = NetworkUtils.getResponseFromHttpUrl(myUrl);
-        } catch (IOException e){
-            Log.e("Main Activity", "Problem making the HTTP request.", e);
+
+        @Override
+        protected String doInBackground(URL... params) {
+
+
+            try {
+                myString = NetworkUtils.getResponseFromHttpUrl(myUrl);
+            } catch (IOException e) {
+                Log.e("Main Activity", "Problem making the HTTP request.", e);
+            }
+
+            Log.i("Information", myString);
+            return myString;
         }
 
-        Log.i("Information", myString);
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+
+            if (myString == null) {
+                return;
+            }
+        }
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
