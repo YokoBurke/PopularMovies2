adfe46feec4bcd1a6f0df3e9e6d4edb1d9e11953
YBURKE <fujikurock@gmail.com>
2018-06-20 23:06:06 -0400

    menu

---
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/JsonUtils.java
@@ -0,0 +1,10 @@
+package com.example.jamesburke.popularmovies.utilities;
+
+/**
+ * Created by jamesburke on 6/19/18.
+ */
+
+public class JsonUtils {
+
+    public static String CLASS_NAME = JsonUtils.class.getSimpleName();
+}
