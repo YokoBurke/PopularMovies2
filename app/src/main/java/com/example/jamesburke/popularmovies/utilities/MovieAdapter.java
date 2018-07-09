448babead8491f10e0ebf922d81a494cd0f42d92
YBURKE <fujikurock@gmail.com>
2018-07-08 21:52:47 -0400

    Picasso working

--- app/src/main/java/com/example/jamesburke/popularmovies/utilities/MovieAdapter.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/MovieAdapter.java
@@ -1,32 +1,41 @@
 package com.example.jamesburke.popularmovies.utilities;
 
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.jamesburke.popularmovies.MainActivity;
 import com.example.jamesburke.popularmovies.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder>{
 
     private List<MovieData> myMovieData;
+    private Context myContext;
 
-    public MovieAdapter(List<MovieData> theMovieData){
+    public MovieAdapter(Context mContext, List<MovieData> theMovieData){
         myMovieData = theMovieData;
+        myContext = mContext;
 
     }
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView myTextView;
+        public ImageView myImageView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             myTextView = (TextView) itemView.findViewById(R.id.info_text);
+            myImageView = (ImageView) itemView.findViewById(R.id.info_image);
         }
 
     }
@@ -45,7 +54,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder
     public void onBindViewHolder(MyViewHolder holder, int position) {
 
         holder.myTextView.setText(myMovieData.get(position).getMyUrl());
-
+        Picasso.with(myContext).load(myMovieData.get(position).getMyUrl()).into(holder.myImageView);
     }
 
     @Override
