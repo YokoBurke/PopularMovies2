a73f9e6eab61e4cf114c9d60aaafb26d0125fa7e
yoko.fujikuro <fujikurock@gmail.com>
2018-07-12 15:48:38 -0400

    all you have to do is format it

--- app/src/main/java/com/example/jamesburke/popularmovies/ChildActivity.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/ChildActivity.java
@@ -7,33 +7,44 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.jamesburke.popularmovies.utilities.MovieData;
+import com.squareup.picasso.Picasso;
 
 public class ChildActivity extends AppCompatActivity {
 
+    private ImageView mBackDrop;
+    private ImageView mPoster;
     private TextView mTitle;
     private TextView mReleaseDate;
     private TextView mVoteAverage;
     private TextView mPlot;
 
-    private ImageView mPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
+        mBackDrop = (ImageView) findViewById(R.id.child_backdrop);
+        mPoster = (ImageView) findViewById(R.id.child_poster);
+
         mTitle = (TextView) findViewById(R.id.child_title);
         mReleaseDate = (TextView) findViewById(R.id.child_release_date);
         mVoteAverage = (TextView) findViewById(R.id.child_vote_average);
         mPlot = (TextView) findViewById(R.id.child_plot);
 
-        mPoster = (ImageView) findViewById(R.id.child_poster);
+
 
         Intent childIntent = getIntent();
         if (childIntent.hasExtra(Intent.EXTRA_TEXT)) {
             MovieData childMovieData = (MovieData) childIntent.getParcelableExtra(Intent.EXTRA_TEXT);
 
+            Picasso.with(this).load(childMovieData.getMyUrl()).into(mPoster);
+            Picasso.with(this).load(childMovieData.getMyBDUrl()).into(mBackDrop);
+
             mTitle.setText(childMovieData.getMyTitle());
+            mReleaseDate.setText(childMovieData.getMyReleaseDate());
+            mVoteAverage.setText(Double.toString(childMovieData.getMyVoteAverage()));
+            mPlot.setText(childMovieData.getMyOverview());
         }
 
     }
