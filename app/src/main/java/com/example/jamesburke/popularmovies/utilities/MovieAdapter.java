65857a41962bb82536f421427952d829bf8faa79
yoko.fujikuro <fujikurock@gmail.com>
2018-08-30 13:28:17 -0400

    Movie adapter commit

--- app/src/main/java/com/example/jamesburke/popularmovies/utilities/MovieAdapter.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/MovieAdapter.java
@@ -3,7 +3,6 @@ package com.example.jamesburke.popularmovies.utilities;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
