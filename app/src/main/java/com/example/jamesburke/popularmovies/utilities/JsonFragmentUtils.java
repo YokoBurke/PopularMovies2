1e0a2450e122972d9f2a5ea740c8f15f0b6d1421
yoko.fujikuro <fujikurock@gmail.com>
2018-09-05 16:58:43 -0400

    Added recyclerview to fragments

---
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/JsonFragmentUtils.java
@@ -0,0 +1,15 @@
+package com.example.jamesburke.popularmovies.utilities;
+
+import java.util.ArrayList;
+
+public class JsonFragmentUtils {
+    private static String CLASS_NAME = JsonFragmentUtils.class.getSimpleName();
+
+    public static ArrayList<MovieReviewsData> parseMovieReviewsData(String json) {
+
+    }
+
+    public static ArrayList<MovieVideosData> parseMovieVideosData(String json) {
+
+    }
+}
