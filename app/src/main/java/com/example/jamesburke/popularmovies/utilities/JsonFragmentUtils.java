5075c5a2700f7b758d936c41e4aa8e0b68752c0d
YBURKE <fujikurock@gmail.com>
2018-09-05 23:11:03 -0400

    JSON Parse Complete

--- app/src/main/java/com/example/jamesburke/popularmovies/utilities/JsonFragmentUtils.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/JsonFragmentUtils.java
@@ -1,15 +1,67 @@
 package com.example.jamesburke.popularmovies.utilities;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class JsonFragmentUtils {
     private static String CLASS_NAME = JsonFragmentUtils.class.getSimpleName();
 
     public static ArrayList<MovieReviewsData> parseMovieReviewsData(String json) {
+        ArrayList<MovieReviewsData> movieReviewsList = new ArrayList<MovieReviewsData>();
 
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(json);
+            JSONArray resultsArray = baseJsonResponse.getJSONArray("results");
+
+            for (int i = 0; i < resultsArray.length(); i++) {
+
+                JSONObject currentMovieReview = resultsArray.getJSONObject(i);
+                String jsonAuthor = currentMovieReview.getString("author");
+                String jsonContent = currentMovieReview.getString("content");
+
+                MovieReviewsData myMovieReviewsData = new MovieReviewsData(jsonAuthor, jsonContent);
+                movieReviewsList.add(myMovieReviewsData);
+            }
+
+
+        } catch (JSONException e) {
+            Log.e(CLASS_NAME, "Problem Parsing MovieReviewsData");
+        }
+
+        return movieReviewsList;
     }
 
     public static ArrayList<MovieVideosData> parseMovieVideosData(String json) {
+        ArrayList<MovieVideosData> movieVideosList = new ArrayList<MovieVideosData>();
+
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(json);
+            JSONArray resultsArray = baseJsonResponse.getJSONArray("results");
+
+            for (int i = 0; i < resultsArray.length(); i++) {
+
+                JSONObject currentMovieReview = resultsArray.getJSONObject(i);
+                String jsonKey = currentMovieReview.getString("key");
+                String jsonTrailorName= currentMovieReview.getString("name");
+                String jsonTrailorSite = currentMovieReview.getString("site");
+
+                MovieVideosData myMovieVideosData = new MovieVideosData(jsonKey, jsonTrailorName, jsonTrailorSite);
+                movieVideosList.add(myMovieVideosData);
+            }
+
+
+        } catch (JSONException e) {
+            Log.e(CLASS_NAME, "Problem Parsing MovieVideosData");
+        }
+
+        return movieVideosList;
 
     }
 }
--- app/src/main/java/com/example/jamesburke/popularmovies/utilities/VideosAdapter.java
+++ app/src/main/java/com/example/jamesburke/popularmovies/utilities/VideosAdapter.java
@@ -1,8 +1,78 @@
 package com.example.jamesburke.popularmovies.utilities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jamesburke.popularmovies.ChildActivity;
+import com.example.jamesburke.popularmovies.R;
+
+import java.util.List;
+
 /**
  * Created by jamesburke on 9/3/18.
  */
 
-public class VideosAdapter {
+public class VideosAdapter extends RecyclerView.Adapter <VideosAdapter.MyVideosViewHolder> {
+
+    private List<MovieVideosData> myMovieVideosData;
+    private Context myContext;
+    final private ListItemClickListener mOnClickListener;
+
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    public VideosAdapter(Context mContext, List<MovieVideosData> theMovieVideosData, VideosAdapter.ListItemClickListener listener) {
+        myMovieVideosData = theMovieVideosData;
+        myContext = mContext;
+        mOnClickListener = listener;
+
+    }
+
+    class MyVideosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView myImageView;
+        public TextView myTextView;
+
+        public MyVideosViewHolder(View itemView) {
+            super(itemView);
+            myImageView = (ImageView) itemView.findViewById(R.id.);
+            myTextView = (TextView) itemView.findViewById(R.id.info_title);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+
+            Intent intent = new Intent(myContext, ChildActivity.class);
+            intent.putExtra(Intent.EXTRA_TEXT, myMovieData.get(clickedPosition));
+            myContext.startActivity(intent);
+        }
+    }
+
+
+    @NonNull
+    @Override
+    public VideosAdapter.MyVideosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideosAdapter.MyVideosViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
